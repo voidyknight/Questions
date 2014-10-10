0904da657209415d76d3852ef33e98d7f6022be6
Applesauce136 <harrison.chiu.136@gmail.com>
2014-10-10 17:19:15 -0400

    Added the app.py file.

--- questions.py
+++ questions.py
@@ -1,10 +1,24 @@
 #regex: [0-9]{1-2}/[0-9]{1-2}/
 #just dates Sept. 22, 2014 or 09/09/09 or 09.09.09 or 09-09-09
+#pythonhosted.org//google/
+
 import re
 import google
-re.M
+from bs4 import BeautifulSoup
+from urllib import urlopen
+
+re.M #Enable multiline searching
 
 question = "When did World War II begin?"
+query_types = ["when", "who"]
 
 num_slash = re.compile("[0,9]{1,2}/[0,9]{1,2}/[0,9]{2,4}");
-google.search(question);
+
+pages = google.search(question,
+              start=1,
+              stop=7,
+              pause=.2
+                  );
+
+url = pages.next()
+print urlopen(url)
