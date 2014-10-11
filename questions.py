a61649d247b151abdab9c8698b7933d6b59c5e83
voidyknight <leonfrajmund@gmail.com>
2014-10-10 20:22:49 -0400

    Encapsulated code.

--- questions.py
+++ questions.py
@@ -1,44 +1,30 @@
-#regex: [0-9]{1-2}/[0-9]{1-2}/
-#just dates Sept. 22, 2014 or 09/09/09 or 09.09.09 or 09-09-09
 #pythonhosted.org//google/
-
-import re
 import google
+import regex
 from bs4 import BeautifulSoup
-
-re.M #Enable multiline searching
 
 question = "When did World War II begin?"
 query_types = ["when", "who"]
 
-regex = re.compile("[0,9]{1,2}/[0,9]{1,2}/[0,9]{2,4}");
-
-pages = google.search(question,
-              start=1,
-              stop=7,
-              pause=.2
-                  );
-
-pages.next()
-rawHTML = google.get_page(pages.next())
-soup = BeautifulSoup(rawHTML)
-paragraph = soup.find_all('p')
-
-
-for index in range(0, len(paragraph)):
-    paragraph[index] = paragraph[index].string
-
-
 numCount = {}
 
-for item in paragraph:
-    list = regex.findall(unicode(item))
-    print item
-    for date in list:
-        if date not in numCount:
-            numCount[date] = 0
-        numCount[date] += 1
+def query(question):
+    pages = google.search(question,
+                          start=1,
+                          stop=7,
+                          pause=.2)
+    return pages
+        
+def parse_all(pages, num_count):
+    (parse(page, numcount) for page in pages)
 
-print numCount
-    
-    
+def parse(page, num_count):
+    paras = get_text(page)
+
+def get_text(page):
+    soup = BeautifulSoup(page)
+    l_tags = soup.find_all('p')
+    paras = [len(l_tags)]
+    for index in range(0, len(l_tags)):
+        paras[index] = unicode(l_tags[index].string)
+    return paras
