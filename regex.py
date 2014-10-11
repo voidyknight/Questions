1f1b4e92cf5f1a02d6e8c7cbee06e9b58e19c4bf
voidyknight <leonfrajmund@gmail.com>
2014-10-10 20:39:58 -0400

    Began object-oriented implementation of regexes.

--- regex.py
+++ regex.py
@@ -6,15 +6,23 @@ d4 = "(\d{4})"
 d2 = "(\d{2})"
 titleword = "([A-Z][a-z]+)"
 
-#BUILT REGEXES
-slashdate_d2 = "%s/%s/%s".format(d2, d2, d2)
-slashdate_d4 = "%s/%s/%s".format(d2, d2, d4)
-worddate_md = "%s %s, %s".format(titleword, d2, d4)
-worddate_dm = "%s %s, %s".format(d2, titleword, d4)
-bc_year = "%s (BC|BCE)".format(d4)
+#BUILT REGEXES    
+class slashdate_d4:
+    regex = "%s/%s/%s".format(d2, d2, d4)
+
+class slashdate_d2:
+    regex = "%s/%s/%s".format(d2, d2, d2)
+
+class worddate_md:
+    regex = "%s %s, %s".format(titleword, d2, d4)
+
+class worddate_dm:
+    regex = "%s %s, %s".format(d2, titleword, d4)
+
+class bc_year:
+    regex  = "%s (BC|BCE)".format(d4)
 
 data = [[[0 for x in range(-4000, 4000)] for x in range(1, 13)] for x in range(1, 32)]
 #data[D][M][Y]
 
 def parse(data, para, regex):
-    
