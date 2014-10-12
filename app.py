4c2114aacf3fbd7f14c24258f71da1a21a8ce0da
voidyknight <leonfrajmund@gmail.com>
2014-10-12 01:26:32 -0400

    Rough code created for flask

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
-import flask
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
 
 @app.route("/")
-def index():
-    render_template("index.html");
+def main():
+
+#names will be changed as css is created
+    question = request.args.get("question", None)
+    sub = request.args.get("submit", None)
+
+    if sub == "submit" and question != None:
+        question = question.replace(" ", "~")
+        return redirect(url_for("answer/"+question))
+    return render_template("main.html");
+
+@app.route("/answer/<question>")
+def answer(question = None):
+    #answer = answer(question)
+    #return render_template("answer.html", ans = answer)
+    pass
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
