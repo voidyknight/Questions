6d094f58e2c472766724c6cb509da1e52cabfd83
voidyknight <leonfrajmund@gmail.com>
2014-10-13 21:44:16 -0400

    got app.py to work

--- app.py
+++ app.py
@@ -2,25 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 import questions
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def questionStuff():
 
 #names will be changed as css is created
-    question = request.args.get("question", None)
-    sub = request.args.get("submit", None)
-
-    if sub == "submit" and question != None:
-        question = question.replace(" ", "~")
-        return redirect(url_for("answer/"+question))
+    if request.method == "POST":
+        question = request.form["question"]
+        if question != None:
+            answer = questions.main(question)
+            sortedict =  sorted(answer, key=answer.get, reverse=True)
+            return render_template("answer.html", keys = sortedict, values = answer.values())
     return render_template("main.html");
-
-@app.route("/answer/<question>")
-def answer(question = None):
-    question = question.replace("~", " ")
-    answer = questions.main(question)
-    sortedict =  sorted(answer, key=answer.get, reverse=True)
-    return render_template("answer.html", keys = sortedict.keys(), values = sortedict.values())
-    pass
 
 @app.route("/about")
 def about():
