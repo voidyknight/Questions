c02dfec51b449aa24215f86c7788797df3d0098b
Applesauce136 <harrison.chiu.136@gmail.com>
2014-10-13 20:24:58 -0400

    did the entire project

--- questions.py
+++ questions.py
@@ -1,30 +1,48 @@
 #pythonhosted.org//google/
 import google
-import regex
+from urllib import urlopen
 from bs4 import BeautifulSoup
 
-question = "When did World War II begin?"
-query_types = ["when", "who"]
+import regex_who
+import regex_when
 
-numCount = {}
+question = "When did World War II start?"
 
-def query(question):
-    pages = google.search(question,
-                          start=1,
-                          stop=7,
-                          pause=.2)
-    return pages
-        
-def parse_all(pages, num_count):
-    (parse(page, numcount) for page in pages)
+def main(question):
+    query_type = get_query_type(question)
+    if query_type == "":
+        return {}
 
-def parse(page, num_count):
-    paras = get_text(page)
+    print "Query Type: " + query_type
+    freq = {}
+    pages = google.search(question, start=1, stop=20, pause=.2)
+    for page in pages:
+        try:
+            print "Reading " + page + "..."
+            fil = urlopen(page)
+            parse(fil.read(), freq, query_type)
+            fil.close()
+        except Exception as e:
+            print e
+    return freq
+
+def get_query_type(question):
+    query_types = ["when", "who"]
+    for query_type in query_types:
+        if query_type in question.lower():
+            return query_type
+    return ""
+
+def parse(page, freq, query_type):
+    text = get_text(page)
+    if query_type == 'who':
+        regex_who.main(freq, text)
+    elif query_type == 'when':
+        regex_when.main(freq, text)
 
 def get_text(page):
     soup = BeautifulSoup(page)
-    l_tags = soup.find_all('p')
-    paras = [len(l_tags)]
-    for index in range(0, len(l_tags)):
-        paras[index] = unicode(l_tags[index].string)
-    return paras
+    return soup.body.get_text(strip=True)
+
+if __name__ == "__main__":
+    print main(question)
